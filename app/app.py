15ad20d6aafe7faec75882ee76f17f85063eef54
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2024-12-26 09:08:57 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import path
 
 # --- Title ---
 st.title("Classification Model for Treatments Prediction")
